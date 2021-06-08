f08c1d30e6b6557089fb40c679e9545ebc35872f
Neelesh <69601292+Cybernite21@users.noreply.github.com>
2021-06-08 12:11:14 -0500

    Made Lobby UI a bit better looking, players can now use a name and see their name on top left corner
    Made Lobby UI a bit better looking, players can now use a name and see their name on top left corner

--- Assets/Scripts/createAndJoinRooms.cs
+++ Assets/Scripts/createAndJoinRooms.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Realtime;
 using Photon.Pun;
 
 public class createAndJoinRooms : MonoBehaviourPunCallbacks
@@ -9,14 +10,25 @@ public class createAndJoinRooms : MonoBehaviourPunCallbacks
 
     public InputField createRoomInput;
     public InputField joinRoomInput;
+    public InputField playerNameInput;
+
+    void Start()
+    {
+        if(PlayerPrefs.GetString("localPlayerName") != null)
+        {
+            playerNameInput.text = PlayerPrefs.GetString("localPlayerName");
+        }
+    }
 
     public void createRoom()
     {
+        storePlayerName();
         PhotonNetwork.CreateRoom(createRoomInput.text);
     }
 
     public void joinRoom()
     {
+        storePlayerName();
         PhotonNetwork.JoinRoom(joinRoomInput.text);
     }
 
@@ -24,6 +36,35 @@ public class createAndJoinRooms : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
+
+        //deal with problem if there is already a player with same name as local player
+        PhotonNetwork.NickName = fixName();
+
+        //load level
         PhotonNetwork.LoadLevel("Game");
+    }
+
+    //stores Player name in playerPrefs
+    void storePlayerName()
+    {
+        PlayerPrefs.SetString("localPlayerName", playerNameInput.text);
+        PlayerPrefs.Save();
+        PhotonNetwork.LocalPlayer.NickName = playerNameInput.text;
+    }
+
+    //add random number to end of name
+    string fixName()
+    {
+        string newLocalPlayerName = PhotonNetwork.NickName;
+
+        foreach (Player p in PhotonNetwork.PlayerListOthers)
+        {
+            if (p.NickName == newLocalPlayerName)
+            {
+                newLocalPlayerName = newLocalPlayerName + Random.Range(0f, 9f);
+            }
+        }
+
+        return newLocalPlayerName;
     }
 }
--- Assets/Scripts/spawnPlayers.cs
+++ Assets/Scripts/spawnPlayers.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 
 public class spawnPlayers : MonoBehaviour
@@ -9,6 +10,8 @@ public class spawnPlayers : MonoBehaviour
     public Vector3 plrSpawnMin;
     public Vector3 plrSpawnMax;
 
+    public Text playerNameText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,8 @@ public class spawnPlayers : MonoBehaviour
         GameObject plr = PhotonNetwork.Instantiate(plrPrefab.name, spawnPos, Quaternion.identity);
         Color randomColor = Random.ColorHSV();
         plr.GetComponent<PhotonView>().RPC("changeColor", RpcTarget.AllBuffered, new Vector3(randomColor.r, randomColor.g, randomColor.b));
+
+        playerNameText.text = "Name: " + PhotonNetwork.NickName;
     }
 
     // Update is called once per frame
